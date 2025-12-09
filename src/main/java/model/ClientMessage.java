003df1bdce743f4ff25351b1c778d39894a3d4d2
nghiado2803 <nghiadvpy00194@gmail.com>
2025-12-09 14:29:47 +0700

    Create ClientMessage.java

---
+++ src/main/java/model/ClientMessage.java
@@ -0,0 +1,19 @@
+package model;
+
+public class ClientMessage {
+    private String username;
+    private String message;
+
+    public ClientMessage() {}
+
+    public ClientMessage(String username, String message) {
+        this.username = username;
+        this.message = message;
+    }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+}
